0d69f4edb0eefbb3c2806c2f08ef5f1edb1302c7
Eric Fiselier <eric@efcs.ca>
2014-06-15 23:09:38 -0600

    add observer stub

--- rapid-cxx-test/observer.hpp
+++ rapid-cxx-test/observer.hpp
@@ -7,6 +7,16 @@
 
 namespace rapid_cxx_test
 {
-    class test_observer;
+    class test_observer
+    {
+    public:
+        test_observer() {}
+    };
+    
+    inline test_observer & get_observer() noexcept
+    {
+        static test_observer o;
+        return o;
+    }
 }                                                   // namespace rapid_cxx_test
 #endif /* RAPID_CXX_TEST_OBSERVER_HPP */
